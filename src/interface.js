9beece86026131ce8944378e78a41af786d84547
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-02-27 21:40:37 +0000

    Creates interface.js file. Uses alert() to establish whether jQuery is working. Click start button - message pops up

---
+++ src/interface.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  let board = new Board();
+
+
+   $("#startgamebutton").on("click", function() {
+    //board.startGame();
+    alert("Player X gets to start")
+   });
+
+   
+});
