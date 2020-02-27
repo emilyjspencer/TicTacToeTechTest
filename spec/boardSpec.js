bbea599cb7573b6c3fb73a981b6ebb9f2b4d90e6
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-02-27 19:27:29 +0000

    Refactors tests to use beforeEach to DRY up code

--- spec/boardSpec.js
+++ spec/boardSpec.js
@@ -1,16 +1,20 @@
 describe("Board", function() {
   let board;
 
+  beforeEach(function() {
+    board = new Board();
+  });
+
   describe('start game', function() {
     it("Player 1 makes the first move", function() {
-      board = new Board;
+      //board = new Board;
       expect(board.currentPlayer()).toEqual("X")
     });
   });
 
   describe('switch turns', function() {
     it("Player 2 makes the second move", function() {
-      board = new Board;
+      //board = new Board;
       board.switchTurn()
       expect(board.currentPlayer()).toEqual("O")
     });
