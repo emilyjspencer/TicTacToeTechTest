ca94c8869faf155be1c3242f2b32ccb0b8717150
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-02-27 19:24:46 +0000

    Player 2 makes the second move. Players can switch turns

--- spec/boardSpec.js
+++ spec/boardSpec.js
@@ -1,9 +1,19 @@
 describe("Board", function() {
   let board;
 
-  it("Player 1 makes the first move", function() {
-    board = new Board;
-    expect(board.currentPlayer()).toEqual("X")
-  })
+  describe('start game', function() {
+    it("Player 1 makes the first move", function() {
+      board = new Board;
+      expect(board.currentPlayer()).toEqual("X")
+    });
+  });
+
+  describe('switch turns', function() {
+    it("Player 2 makes the second move", function() {
+      board = new Board;
+      board.switchTurn()
+      expect(board.currentPlayer()).toEqual("O")
+    });
+  });
 
 });
--- src/board.js
+++ src/board.js
@@ -1,8 +1,15 @@
 function Board() {
   this.player = "X"
-
-};
+}; 
 
 Board.prototype.currentPlayer = function() {
   return this.player;
 }
+
+Board.prototype.switchTurn = function() {
+  if(this.player == "X"){
+    this.player = "O"
+  } else if(this.player == "O"){
+    this.player == "X"
+  }
+};
