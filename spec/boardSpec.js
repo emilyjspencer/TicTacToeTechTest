7c681c3a43baa3a2d69c7f4d1bf89818e8589a22
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-02-27 19:27:54 +0000

    Removes dead code

--- spec/boardSpec.js
+++ spec/boardSpec.js
@@ -7,14 +7,12 @@ describe("Board", function() {
 
   describe('start game', function() {
     it("Player 1 makes the first move", function() {
-      //board = new Board;
       expect(board.currentPlayer()).toEqual("X")
     });
   });
 
   describe('switch turns', function() {
     it("Player 2 makes the second move", function() {
-      //board = new Board;
       board.switchTurn()
       expect(board.currentPlayer()).toEqual("O")
     });
